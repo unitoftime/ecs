e826fef0dbf8c4dd13544e37c2e9437653353f35
Jacob Stewart <2606873+unitoftime@users.noreply.github.com>
2022-12-13 17:47:00 -0500

    Added map5

--- view.go
+++ view.go
@@ -209,6 +209,56 @@ func Map4[A, B, C, D any](world *World, lambda func(id Id, a *A, b *B, c *C, d *
 	}
 }
 
+func Map5[A, B, C, D, E any](world *World, lambda func(id Id, a *A, b *B, c *C, d *D, e *E)) {
+	var a A
+	var b B
+	var c C
+	var d D
+	var e E
+	archIds := world.engine.Filter(a, b, c, d, e)
+
+	// storages := getAllStorages(world, a)
+	aStorage := GetStorage[A](world.engine)
+	bStorage := GetStorage[B](world.engine)
+	cStorage := GetStorage[C](world.engine)
+	dStorage := GetStorage[D](world.engine)
+	eStorage := GetStorage[E](world.engine)
+
+	for _, archId := range archIds {
+		aSlice, ok := aStorage.slice[archId]
+		if !ok { continue }
+		bSlice, ok := bStorage.slice[archId]
+		if !ok { continue }
+		cSlice, ok := cStorage.slice[archId]
+		if !ok { continue }
+		dSlice, ok := dStorage.slice[archId]
+		if !ok { continue }
+		eSlice, ok := eStorage.slice[archId]
+		if !ok { continue }
+
+		lookup, ok := world.engine.lookup[archId]
+		if !ok { panic("LookupList is missing!") }
+
+		ids := lookup.id
+		aComp := aSlice.comp
+		bComp := bSlice.comp
+		cComp := cSlice.comp
+		dComp := dSlice.comp
+		eComp := eSlice.comp
+		if len(ids) != len(aComp) || len(ids) != len(bComp) || len(ids) != len(cComp) || len(ids) != len(dComp) || len(ids) != len(eComp){
+			panic("ERROR - Bounds don't match")
+		}
+		for i := range ids {
+			if ids[i] == InvalidEntity { continue }
+			lambda(ids[i], &aComp[i], &bComp[i], &cComp[i], &dComp[i], &eComp[i])
+		}
+		// for i, id := range lookup.id {
+		// 	if id == InvalidEntity { continue }
+		// 	lambda(lookup.id[i], &aSlice.comp[i], &bSlice.comp[i], &cSlice.comp[i], &dSlice.comp[i])
+		// }
+	}
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // type SliceReader[T any] interface {
 // 	Read(int) T
